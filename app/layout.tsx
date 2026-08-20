2fb86db594b5d1f796c685d4140a32b6ac032be4
takeshi0518 <y.takeshi0518@gmail.com>
2026-08-20 18:00:57 +0900

    ch04-creating-layouts-and-pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+    <html lang="ja">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
